1750c3217ed41fb27a6a62263c262abc7963fad3
atharvanaphade <naphade21@gmail.com>
2020-10-31 00:51:48 +0530

    changes~

---
+++ Templates/main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception{
+		Scanner testcases = new Scanner(System.in);
+		int i = testcases.nextInt();
+		while(i != 0){
+		    Solve.solve();
+		    i--;
+		}
+	}
+}
+
+class Solve{
+    static void solve(){
+        //Logic Here
+    }
+}
--- cp.py
+++ cp.py
@@ -1,11 +1,11 @@
 import os
-printf("-------------------------------Competetive Programming Assistant---------------------------------------")
-printf("Enter the Contest Name : ")
+print("-------------------------------Competetive Programming Assistant---------------------------------------")
+print("Enter the Contest Name : ")
 contest_name = str(input())
-printf("Enter the Number of Directories : ")
+print("Enter the Number of Directories : ")
 number = int(intput())
 for i in range(number):
-    printf("Enter Name of Problem : ")
+    print("Enter Name of Problem : ")
     problem = str(input())
     os.mkdir("G:\Contests\" + contest_name + "\" + problem)
     code_file = open("G:\Contests" + contest_name + "\" + problem + "\" + problem + ".cpp", "w+")
